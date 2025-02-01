d68b015a911d1b98a9e13331b9dcb3ebe3d3f3dd
Theov3r <dominik.rybczynski00@outlook.com>
2025-02-01 18:49:48 +0100

    Add files via upload

---
+++ skrypt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir "S33522index"
+mkdir "poprawa" ~/s33522index
+mkdir -p "dokumenty" "obrazy" "zadanie"  ~/s33522/s33522index
+mkdir -p "tekstowe" "word"  ~/s33522/s33522index/dokumenty
+mkdir -p "master" "test"  ~/s33522/s33522index/zadanie
+touch "plik1.txt"  ~/s33522/s33522index/dokumenty/tekstowe
+
+touch "plik2.txt"  ~/s33522/s33522index/dokumenty/word
+
+touch "plik3.txt"  ~/s33522/s33522index/obrazy
+touch "plik4.txt"  ~/s33522/s33522index/zadanie/master
+touch "plik5.txt"  ~/s33522/s33522index/zadanie/test
+
+echo -p Biedronka Kawa 25zl
+Auchan Woda 2zl
+Lidl Chleb 6zl
+Biedronka Jajka 12zl
+Auchan Kurczak 24zl
+Lidl Mleko 4zl
+Biedronka Ser 10zl
+Auchan Ziemniaki 7zl
+Lidl Czekolada 5zl
+Biedronka Pomidory 11zl
+Auchan Makaron 6zl
+Lidl Banany 6zl
+Biedronka Jogurt 5zl
+Auchan Cebula 4zl
+Lidl Maslo 8zl
+Biedronka Szynka 15zl
+Auchan Ryz 9zl
+Lidl Sok 7zl
+Biedronka Cukier 5zl
+Auchan Papier 14z > ~/s33522/s33522index/dokumenty/tekstowe/plik1.txt
